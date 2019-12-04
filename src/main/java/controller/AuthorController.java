2bfd76658ed06077921bc802b1ae59b223b0669d
Zhongy95 <1007363681@qq.com>
2019-12-04 14:27:23 +0800

    添加redis-Util类

--- src/main/java/controller/AuthorController.java
+++ src/main/java/controller/AuthorController.java
@@ -2,6 +2,7 @@ package controller;
 
 import Bean.QueryResponse;
 import Services.PubmedSearch;
+import com.mchange.v2.log.MLogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -16,6 +17,7 @@ public class AuthorController {
     private PubmedSearch search;
     @RequestMapping(value = "/getAuthor")
     public String searchAuthor(String query){
+        MLogger log = null;
         log.info("get query:"+query);
         QueryResponse response=search.processPubmedSearch(query);
         log.info("get the result:"+response.getResult());
